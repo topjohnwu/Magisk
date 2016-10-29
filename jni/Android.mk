3255ca3ea47e97d723a477321e64b21ee98dd70c
topjohnwu <topjohnwu@gmail.com>
2016-10-30 06:08:23 +0800

    Remove unnecessary statics

--- jni/Android.mk
+++ jni/Android.mk
@@ -6,8 +6,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := magiskhide
 LOCAL_MODULE_TAGS := optional
 LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_LDFLAGS := -static
-LOCAL_STATIC_LIBRARIES := libc libcutils
 LOCAL_SRC_FILES := magiskhide.c
 include $(BUILD_EXECUTABLE)
 
@@ -15,8 +13,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := bootimgtools
 LOCAL_MODULE_TAGS := optional
 LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_LDFLAGS := -static
-LOCAL_STATIC_LIBRARIES := libc libcutils
 LOCAL_SRC_FILES := bootimgtools.c extract.c repack.c hexpatch.c
 LOCAL_CFLAGS += -std=gnu11
 include $(BUILD_EXECUTABLE)
@@ -25,8 +21,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := sepolicy-inject
 LOCAL_MODULE_TAGS := optional
 LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_LDFLAGS := -static
-LOCAL_STATIC_LIBRARIES := libc libcutils libsepol
+LOCAL_STATIC_LIBRARIES := libsepol
 LOCAL_SRC_FILES := sepolicy-inject/sepolicy-inject.c sepolicy-inject/builtin_rules.c
 LOCAL_C_INCLUDES := selinux/libsepol/include/
 LOCAL_CFLAGS += -std=gnu11
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,3 +1,3 @@
-APP_ABI := x86 armeabi
+APP_ABI := x86 x86_64 armeabi arm64-v8a
 APP_PIE = true
 APP_PLATFORM := android-21
