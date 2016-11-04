1d0c36a0abe8ca5a35276cf6434439985fd69a16
topjohnwu <topjohnwu@gmail.com>
2016-11-05 02:35:23 +0800

    Add resetprop to Magisk

--- jni/Android.mk
+++ jni/Android.mk
@@ -19,11 +19,17 @@ include $(BUILD_EXECUTABLE)
 include $(CLEAR_VARS)
 LOCAL_MODULE := sepolicy-inject
 LOCAL_MODULE_TAGS := optional
-LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES := libsepol
 LOCAL_SRC_FILES := sepolicy-inject/sepolicy-inject.c sepolicy-inject/builtin_rules.c
 LOCAL_C_INCLUDES := selinux/libsepol/include/
 LOCAL_CFLAGS += -std=gnu11
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := resetprop
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := resetprop/resetprop.cpp resetprop/system_properties.cpp resetprop/libc_logging.cpp
+LOCAL_LDLIBS += -latomic
 include $(BUILD_EXECUTABLE)
 
 include selinux/libsepol/Android.mk
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,3 +1,5 @@
 APP_ABI := x86 x86_64 armeabi arm64-v8a
 APP_PIE = true
 APP_PLATFORM := android-21
+APP_CPPFLAGS += -std=c++11
+# APP_STL := c++_static
