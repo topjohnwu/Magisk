99406f2099fa0406142e9a625afc174aae2ac08d
topjohnwu <topjohnwu@gmail.com>
2016-12-30 06:06:19 +0800

    Separate all Android.mk files

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,6 +1,4 @@
-my_path := $(call my-dir)
-
-LOCAL_PATH := $(my_path)
+LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := magiskhide
@@ -16,30 +14,9 @@ LOCAL_SRC_FILES := bootimgtools/main.c bootimgtools/extract.c bootimgtools/repac
 LOCAL_CFLAGS += -std=gnu11
 include $(BUILD_EXECUTABLE)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := sepolicy-inject
-LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_LIBRARIES := libsepol
-LOCAL_SRC_FILES := sepolicy-inject/sepolicy-inject.c sepolicy-inject/builtin_rules.c
-LOCAL_C_INCLUDES := jni/selinux/libsepol/include/
-LOCAL_CFLAGS += -std=gnu11
-include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := resetprop
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := resetprop/resetprop.cpp resetprop/system_properties.cpp resetprop/libc_logging.cpp
-LOCAL_LDLIBS += -latomic
-include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := su
-LOCAL_MODULE_TAGS := eng debug optional
-LOCAL_STATIC_LIBRARIES := libselinux
-LOCAL_C_INCLUDES := jni/selinux/libselinux/include/ jni/selinux/libsepol/include/ jni/su/sqlite3/
-LOCAL_SRC_FILES := su/su.c su/daemon.c su/activity.c su/db.c su/utils.c su/pts.c su/hacks.c su/binds.c su/sqlite3/sqlite3.c
-LOCAL_CFLAGS := -DSQLITE_OMIT_LOAD_EXTENSION -std=gnu11
-include $(BUILD_EXECUTABLE)
+include jni/resetprop/Android.mk
+include jni/sepolicy-inject/Android.mk
+include jni/su/Android.mk
 
 include jni/selinux/libsepol/Android.mk
 include jni/selinux/libselinux/Android.mk
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,6 +1,4 @@
-NDK_TOOLCHAIN_VERSION=4.9
 APP_ABI := x86 x86_64 armeabi arm64-v8a
 APP_PIE = true
 APP_PLATFORM := android-21
 APP_CPPFLAGS += -std=c++11
-# APP_STL := c++_static
