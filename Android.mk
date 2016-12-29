f31d2486c91be0664012be60936e09ef4fab2bb0
topjohnwu <topjohnwu@gmail.com>
2016-12-30 06:03:02 +0800

    Add Android.mk

---
+++ Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := su
+LOCAL_MODULE_TAGS := eng debug optional
+LOCAL_STATIC_LIBRARIES := libselinux
+LOCAL_C_INCLUDES := jni/selinux/libselinux/include/ jni/selinux/libsepol/include/ jni/su/sqlite3/
+LOCAL_SRC_FILES := su.c daemon.c activity.c db.c utils.c pts.c hacks.c sqlite3/sqlite3.c
+LOCAL_CFLAGS := -DSQLITE_OMIT_LOAD_EXTENSION -std=gnu11
+include $(BUILD_EXECUTABLE)
