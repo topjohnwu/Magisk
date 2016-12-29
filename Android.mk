7dea682713cc81532291c8e8365b13b5a592e9c6
topjohnwu <topjohnwu@gmail.com>
2016-12-30 06:02:26 +0800

    Add Android.mk

---
+++ Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := sepolicy-inject
+LOCAL_MODULE_TAGS := optional
+LOCAL_STATIC_LIBRARIES := libsepol
+LOCAL_SRC_FILES := sepolicy-inject.c builtin_rules.c
+LOCAL_C_INCLUDES := jni/selinux/libsepol/include/
+LOCAL_CFLAGS += -std=gnu11
+include $(BUILD_EXECUTABLE)
