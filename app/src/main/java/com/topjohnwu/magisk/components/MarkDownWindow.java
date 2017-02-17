9bc410dd3d85c8fc1f725a2a82bc66a2dcab59af
topjohnwu <topjohnwu@gmail.com>
2017-02-18 04:35:51 +0800

    Add MarkDown styles

--- app/src/main/java/com/topjohnwu/magisk/components/MarkDownWindow.java
+++ app/src/main/java/com/topjohnwu/magisk/components/MarkDownWindow.java
@@ -3,21 +3,25 @@ package com.topjohnwu.magisk.components;
 import android.content.Context;
 import android.support.v7.app.AlertDialog;
 
+import com.topjohnwu.magisk.MagiskManager;
 import com.topjohnwu.magisk.R;
 import com.topjohnwu.magisk.utils.Logger;
+import com.topjohnwu.magisk.utils.Utils;
 
 import us.feras.mdv.MarkdownView;
 
 public class MarkDownWindow {
 
     public MarkDownWindow(String title, String url, Context context) {
-        AlertDialog.Builder alert = new AlertDialogBuilder(context);
+        MagiskManager magiskManager = Utils.getMagiskManager(context);
+        AlertDialog.Builder alert = new AlertDialog.Builder(context);
         alert.setTitle(title);
     
         Logger.dev("WebView: URL = " + url);
 
         MarkdownView md = new MarkdownView(context);
-        md.loadMarkdownFile(url);
+        md.loadMarkdownFile(url, "file:///android_asset/" +
+                (magiskManager.isDarkTheme ? "dark" : "light") + ".css");
 
         alert.setView(md);
         alert.setNegativeButton(R.string.close, (dialog, id) -> dialog.dismiss());
