1437c5c63f0fb2c58e6bd2432e18b33d7c625207
topjohnwu <topjohnwu@gmail.com>
2017-02-28 17:24:27 +0800

    Add ndk-compression with liblzma

--- jni/Android.mk
+++ jni/Android.mk
@@ -6,5 +6,7 @@ include jni/resetprop/Android.mk
 include jni/sepolicy-inject/Android.mk
 include jni/su/Android.mk
 
+# Libraries
 include jni/selinux/libsepol/Android.mk
 include jni/selinux/libselinux/Android.mk
+include jni/ndk-compression/xz/src/liblzma/Android.mk
