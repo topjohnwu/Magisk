eb38393cad748b3b1e3ce424843f42614835de53
topjohnwu <topjohnwu@gmail.com>
2017-02-28 17:46:10 +0800

    Cleanup Android.mk

--- Android.mk
+++ Android.mk
@@ -2,7 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := su
-LOCAL_MODULE_TAGS := eng debug optional
 LOCAL_STATIC_LIBRARIES := libselinux
 LOCAL_C_INCLUDES := jni/selinux/libselinux/include/ jni/selinux/libsepol/include/ jni/su/sqlite3/
 LOCAL_SRC_FILES := su.c daemon.c activity.c db.c utils.c pts.c sqlite3/sqlite3.c
