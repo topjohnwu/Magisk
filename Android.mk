2fe4d9706148d19c4ff2dce9119f40ae0641925a
topjohnwu <topjohnwu@gmail.com>
2017-02-28 17:49:23 +0800

    Cleanup Android.mk

--- Android.mk
+++ Android.mk
@@ -2,7 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := sepolicy-inject
-LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_LIBRARIES := libsepol
 LOCAL_SRC_FILES := main.c sepolicy.c rules.c utils.c
 LOCAL_C_INCLUDES := jni/selinux/libsepol/include/
