ff48996bbeadfefabd74d2741d9fa71992d87eda
topjohnwu <topjohnwu@gmail.com>
2017-03-02 04:07:39 +0800

    Add libselinux

--- Android.mk
+++ Android.mk
@@ -7,3 +7,5 @@ LOCAL_C_INCLUDES := jni/selinux/libselinux/include/ jni/selinux/libsepol/include
 LOCAL_SRC_FILES := su.c daemon.c activity.c db.c utils.c pts.c sqlite3/sqlite3.c
 LOCAL_CFLAGS := -DSQLITE_OMIT_LOAD_EXTENSION -std=gnu11
 include $(BUILD_EXECUTABLE)
+
+include jni/selinux/libselinux/Android.mk
