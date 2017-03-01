9f35fa0fa3f9b52c2b6b31253bade848019d7ab2
topjohnwu <topjohnwu@gmail.com>
2017-03-02 04:08:04 +0800

    Add libsepol

--- Android.mk
+++ Android.mk
@@ -7,3 +7,5 @@ LOCAL_SRC_FILES := main.c sepolicy.c rules.c utils.c
 LOCAL_C_INCLUDES := jni/selinux/libsepol/include/
 LOCAL_CFLAGS += -std=gnu11
 include $(BUILD_EXECUTABLE)
+
+include jni/selinux/libsepol/Android.mk
