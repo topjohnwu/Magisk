65dc99744ee34c9e58ed1f04ff8e138e2bcf9bfa
topjohnwu <topjohnwu@gmail.com>
2017-03-04 21:18:24 +0800

    Use our own zlib for static link and ZLIB_CONST flag

--- jni/magiskboot/Android.mk
+++ jni/magiskboot/Android.mk
@@ -2,15 +2,15 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := magiskboot
-LOCAL_STATIC_LIBRARIES := liblzma liblz4 libbz2
+LOCAL_STATIC_LIBRARIES := libz liblzma liblz4 libbz2
 LOCAL_C_INCLUDES := \
 	jni/ndk-compression/zlib/ \
 	jni/ndk-compression/xz/src/liblzma/api/ \
 	jni/ndk-compression/lz4/lib/ \
 	jni/ndk-compression/bzip2/
 
-LOCAL_SRC_FILES := main.c unpack.c repack.c hexpatch.c parseimg.c compress.c utils.c cpio.c
-LOCAL_LDLIBS += -lz
+LOCAL_SRC_FILES := main.c unpack.c repack.c hexpatch.c parseimg.c compress.c utils.c
+LOCAL_CFLAGS += -DZLIB_CONST
 include $(BUILD_EXECUTABLE)
 
 include jni/ndk-compression/zlib/Android.mk
