3865acceb0af7417679235badd1649fe442a5fc4
Alex <good.win.alexs@gmail.com>
2017-07-11 11:07:17 +0300

    Support for A/B devices (like Pixel family)

--- jni/magiskhide/hide_utils.c
+++ jni/magiskhide/hide_utils.c
@@ -60,31 +60,64 @@ void hide_sensitive_props() {
 
 void relink_sbin() {
 	struct stat st;
+	DIR *dir;
+	struct dirent *entry;
+	char from[PATH_MAX], to[PATH_MAX];
+
 	if (stat("/sbin_orig", &st) == -1 && errno == ENOENT) {
 		// Re-link all binaries and bind mount 
-		DIR *dir;
-		struct dirent *entry;
-		char from[PATH_MAX], to[PATH_MAX];
 
 		LOGI("hide_utils: Re-linking /sbin\n");
 
 		xmount(NULL, "/", NULL, MS_REMOUNT, NULL);
-		xrename("/sbin", "/sbin_orig");
-		xmkdir("/sbin", 0755);
-		xchmod("/sbin", 0755);
-		xmount(NULL, "/", NULL, MS_REMOUNT | MS_RDONLY, NULL);
-		xmkdir("/dev/sbin_bind", 0755);
-		xchmod("/dev/sbin_bind", 0755);
-		dir = xopendir("/sbin_orig");
 
-		while ((entry = xreaddir(dir))) {
-			snprintf(from, sizeof(from), "%s/%s", "/sbin_orig", entry->d_name);
-			snprintf(to, sizeof(to), "%s/%s", "/dev/sbin_bind", entry->d_name);
-			symlink(from, to);
-			lsetfilecon(to, "u:object_r:system_file:s0");
+		clone_dir("/sbin", "/sbin_orig");
+		xchmod("/sbin_orig", 0755);
+		unlink("/sbin/magiskpolicy");
+		unlink("/sbin/sepolicy-inject");
+		unlink("/sbin/resetprop");
+		unlink("/sbin/su");
+		unlink("/sbin/supolicy");
+
+		xmount(NULL, "/", NULL, MS_REMOUNT | MS_RDONLY, NULL);
+
+		if (stat("/dev/sbin_bind", &st) == -1 && errno == ENOENT) {
+			xmkdir("/dev/sbin_bind", 0755);
+			xchmod("/dev/sbin_bind", 0755);
+
+			dir = xopendir("/sbin_orig");
+			while ((entry = xreaddir(dir))) {
+				snprintf(from, sizeof(from), "%s/%s", "/sbin_orig", entry->d_name);
+				snprintf(to, sizeof(to), "%s/%s", "/dev/sbin_bind", entry->d_name);
+				symlink(from, to);
+				lsetfilecon(to, "u:object_r:system_file:s0");
+			}
+			closedir(dir);
 		}
-		
-		closedir(dir);
+		xmount("/dev/sbin_bind", "/sbin", NULL, MS_BIND, NULL);
+	}else{
+		LOGI("hide_utils: /sbin_orig exists, processing...\n");
+		xmount(NULL, "/", NULL, MS_REMOUNT, NULL);
+		unlink("/sbin/magiskpolicy");
+		unlink("/sbin/sepolicy-inject");
+		unlink("/sbin/resetprop");
+		unlink("/sbin/su");
+		unlink("/sbin/supolicy");
+		xmount(NULL, "/", NULL, MS_REMOUNT | MS_RDONLY, NULL);
+
+		if (stat("/dev/sbin_bind", &st) == -1 && errno == ENOENT) {
+			xmkdir("/dev/sbin_bind", 0755);
+			xchmod("/dev/sbin_bind", 0755);
+
+			dir = xopendir("/sbin_orig");
+			while ((entry = xreaddir(dir))) {
+				snprintf(from, sizeof(from), "%s/%s", "/sbin_orig", entry->d_name);
+				snprintf(to, sizeof(to), "%s/%s", "/dev/sbin_bind", entry->d_name);
+				symlink(from, to);
+				lsetfilecon(to, "u:object_r:system_file:s0");
+			}
+			closedir(dir);
+		}
 
 		xmount("/dev/sbin_bind", "/sbin", NULL, MS_BIND, NULL);
 	}
