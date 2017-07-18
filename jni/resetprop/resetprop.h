5b36b4472c529728a4cd2bc99be1cb493bc30111
topjohnwu <topjohnwu@gmail.com>
2017-07-18 11:56:57 +0800

    Update and add new resetprop features

--- jni/resetprop/resetprop.h
+++ jni/resetprop/resetprop.h
@@ -12,8 +12,9 @@ int prop_exist(const char *name);
 int setprop(const char *name, const char *value);
 int setprop2(const char *name, const char *value, const int trigger);
 char *getprop(const char *name);
-int deleteprop(const char *name);
+int deleteprop(const char *name, const int trigger);
 int read_prop_file(const char* filename, const int trigger);
+void getprop_all(void (*cbk)(const char *name));
 
 #ifdef __cplusplus
 }
