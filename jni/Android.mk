64e85da59f5f029144a2b65b286fb7fa0122c0ce
topjohnwu <topjohnwu@gmail.com>
2017-07-31 23:47:50 +0800

    Merge Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,5 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
+########################
+# Binaries
+########################
+
+# magisk main binary
 include $(CLEAR_VARS)
 LOCAL_MODULE := magisk
 LOCAL_STATIC_LIBRARIES := libsepol
@@ -41,21 +46,53 @@ LOCAL_SRC_FILES := \
 LOCAL_CFLAGS := -Wno-implicit-exception-spec-mismatch
 LOCAL_CPPFLAGS := -std=c++11
 LOCAL_LDLIBS := -llog
-
 include $(BUILD_EXECUTABLE)
 
-# External shared libraries, build stub libraries for linking
-include jni/external/Android.mk
+# magiskboot
+include $(CLEAR_VARS)
+LOCAL_MODULE := magiskboot
+LOCAL_STATIC_LIBRARIES := libz liblzma liblz4 libbz2
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/magiskboot \
+	$(LOCAL_PATH)/include \
+	$(LOCAL_PATH)/ndk-compression/zlib \
+	$(LOCAL_PATH)/ndk-compression/xz/src/liblzma/api \
+	$(LOCAL_PATH)/ndk-compression/lz4/lib \
+	$(LOCAL_PATH)/ndk-compression/bzip2
 
-# libsepol, static library
-include jni/selinux/libsepol/Android.mk
+LOCAL_SRC_FILES := \
+	magiskboot/main.c \
+	magiskboot/bootimg.c \
+	magiskboot/hexpatch.c \
+	magiskboot/compress.c \
+	magiskboot/boot_utils.c \
+	magiskboot/cpio.c \
+	magiskboot/sha1.c \
+	utils/xwrap.c \
+	utils/vector.c \
+	utils/list.c
+LOCAL_CFLAGS := -DZLIB_CONST
+include $(BUILD_EXECUTABLE)
 
-# Build magiskboot
-include jni/magiskboot/Android.mk
-
-# Build busybox (32-bit only)
+# busybox (32-bit only)
 ifneq ($(TARGET_ARCH_ABI), x86_64)
 ifneq ($(TARGET_ARCH_ABI), arm64-v8a)
 include jni/busybox/Android.mk
 endif
 endif
+
+########################
+# Libraries
+########################
+
+# External shared libraries, include stub libselinux and libsqlite
+include jni/external/Android.mk
+
+# libsepol, static library
+include jni/selinux/libsepol/Android.mk
+
+# Compression libraries for magiskboot
+include jni/ndk-compression/zlib/Android.mk
+include jni/ndk-compression/xz/src/liblzma/Android.mk
+include jni/ndk-compression/lz4/lib/Android.mk
+include jni/ndk-compression/bzip2/Android.mk
