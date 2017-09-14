8516ebe6f57424bf5f2eb801dd03386f0b043f17
topjohnwu <topjohnwu@gmail.com>
2017-09-14 10:46:32 +0800

    Add libfdt

--- jni/external/Android.mk
+++ jni/external/Android.mk
@@ -14,6 +14,22 @@ LOCAL_C_INCLUDES := $(LIBSELINUX)
 LOCAL_SRC_FILES := stubs/selinux_stub.c
 include $(BUILD_SHARED_LIBRARY)
 
+# libfdt
+include $(CLEAR_VARS)
+LOCAL_MODULE:= libfdt
+LOCAL_C_INCLUDES := $(LIBFDT)
+LOCAL_SRC_FILES := \
+	dtc/libfdt/fdt.c \
+	dtc/libfdt/fdt_addresses.c \
+	dtc/libfdt/fdt_empty_tree.c \
+	dtc/libfdt/fdt_overlay.c \
+	dtc/libfdt/fdt_ro.c \
+	dtc/libfdt/fdt_rw.c \
+	dtc/libfdt/fdt_strerror.c \
+	dtc/libfdt/fdt_sw.c \
+	dtc/libfdt/fdt_wip.c
+include $(BUILD_STATIC_LIBRARY)
+
 # libsepol, static library
 include $(SELINUX_PATH)/libsepol/Android.mk
 
