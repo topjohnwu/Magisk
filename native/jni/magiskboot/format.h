31360c34edbde1301bda821d93ab0a6c5fcbcd0b
worstperson <xmoser@gmail.com>
2018-02-22 05:10:44 -0600

    Set NOOK_PRE_HEADER_SZ from 0xFFFFF to 0x100000
    All applicable Nook HD/HD+ roms are using this offset

--- native/jni/magiskboot/format.h
+++ native/jni/magiskboot/format.h
@@ -39,7 +39,7 @@ typedef enum {
 #define SEANDROID_MAGIC "SEANDROIDENFORCE"
 #define TEGRABLOB_MAGIC "-SIGNED-BY-SIGNBLOB-"
 #define NOOK_MAGIC      "Green Loader"
-#define NOOK_PRE_HEADER_SZ 1048575
+#define NOOK_PRE_HEADER_SZ 1048576
 
 #define SUP_LIST      ((char *[]) { "gzip", "xz", "lzma", "bzip2", "lz4", "lz4_legacy", NULL })
 #define SUP_EXT_LIST  ((char *[]) { "gz", "xz", "lzma", "bz2", "lz4", "lz4", NULL })
