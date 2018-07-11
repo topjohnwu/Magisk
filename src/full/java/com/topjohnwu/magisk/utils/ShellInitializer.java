68c448bc34b55587d30ae21ce816114402f16b75
topjohnwu <topjohnwu@gmail.com>
2018-07-11 20:44:29 +0800

    Let ShellInitializer run in BusyBox environment

--- src/full/java/com/topjohnwu/magisk/utils/ShellInitializer.java
+++ src/full/java/com/topjohnwu/magisk/utils/ShellInitializer.java
@@ -6,20 +6,26 @@ import android.support.annotation.NonNull;
 import com.topjohnwu.magisk.R;
 import com.topjohnwu.superuser.BusyBox;
 import com.topjohnwu.superuser.Shell;
+import com.topjohnwu.superuser.ShellUtils;
 
 import java.io.File;
 import java.io.InputStream;
 
 public class ShellInitializer extends Shell.Initializer {
 
+    static {
+        BusyBox.BB_PATH = new File(Const.BUSYBOX_PATH);
+    }
+
     @Override
     public boolean onRootShellInit(Context context, @NonNull Shell shell) throws Exception {
-        BusyBox.BB_PATH = new File(Const.BUSYBOX_PATH);
         try (InputStream magiskUtils = context.getResources().openRawResource(R.raw.util_functions);
              InputStream managerUtils = context.getResources().openRawResource(R.raw.utils)
         ) {
-            shell.loadInputStream(null, null, magiskUtils);
-            shell.loadInputStream(null, null, managerUtils);
+            shell.execTask((in, err, out) -> {
+                ShellUtils.pump(magiskUtils, in);
+                ShellUtils.pump(managerUtils, in);
+            });
         }
         shell.run(null, null,
                 "mount_partitions",
