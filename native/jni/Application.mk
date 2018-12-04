68b07c591342b594bf2eaeb2bed5951ca31d1473
topjohnwu <topjohnwu@gmail.com>
2018-12-03 19:43:02 -0500

    Use flags for smaller binary

--- native/jni/Application.mk
+++ native/jni/Application.mk
@@ -1,5 +1,6 @@
 APP_ABI := armeabi-v7a x86
-APP_CFLAGS := -std=gnu11 -DMAGISK_VERSION="${MAGISK_VERSION}" -DMAGISK_VER_CODE=${MAGISK_VER_CODE}
+APP_CFLAGS := -Oz -std=gnu11 \
+-DMAGISK_VERSION="${MAGISK_VERSION}" -DMAGISK_VER_CODE=${MAGISK_VER_CODE}
 APP_CPPFLAGS := -std=c++14
 APP_STL := system
 APP_PLATFORM := android-16
@@ -10,6 +11,7 @@ endif
 
 # Busybox require some additional settings
 ifdef B_BB
+APP_CFLAGS := -Os
 APP_SHORT_COMMANDS := true
 NDK_TOOLCHAIN_VERSION := 4.9
 APP_PLATFORM := android-21
