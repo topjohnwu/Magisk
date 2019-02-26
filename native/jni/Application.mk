5764f0c8394d145aa5c1cc81806dc7fa0400e92e
topjohnwu <topjohnwu@gmail.com>
2019-02-26 03:42:44 -0500

    Compiler flag enhancements
    - Enable LTO
    - Add -fomit-frame-pointer for even smaller binary size
    
    This commit is inspired by #1075. Close #1075.

--- native/jni/Application.mk
+++ native/jni/Application.mk
@@ -1,6 +1,7 @@
 APP_ABI := armeabi-v7a x86
-APP_CFLAGS := -Oz -std=gnu11 \
+APP_CFLAGS := -Oz -fomit-frame-pointer -flto \
 -D__MVSTR=${MAGISK_VERSION} -D__MCODE=${MAGISK_VER_CODE}
+APP_LDFLAGS := -flto
 APP_CPPFLAGS := -std=c++17
 APP_STL := c++_static
 APP_PLATFORM := android-16
@@ -16,7 +17,7 @@ endif
 
 # Busybox require some additional settings
 ifdef B_BB
-APP_CFLAGS := -Os
+APP_CFLAGS := -Os -fomit-frame-pointer -flto
 APP_SHORT_COMMANDS := true
 NDK_TOOLCHAIN_VERSION := 4.9
 APP_PLATFORM := android-21
