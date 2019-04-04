a4f5d47e72158a559bd4b47e70bf7ba1126b2dbd
vvb2060 <vvb2060@gmail.com>
2019-04-05 05:13:57 +0800

    get_flags need before find_boot_image

--- scripts/flash_script.sh
+++ scripts/flash_script.sh
@@ -44,10 +44,10 @@ ui_print "************************"
 is_mounted /data || mount /data || is_mounted /cache || mount /cache || abort "! Unable to mount partitions"
 mount_partitions
 
-find_boot_image
-
 check_data
 get_flags
+
+find_boot_image
 
 [ -z $BOOTIMAGE ] && abort "! Unable to detect target image"
 ui_print "- Target image: $BOOTIMAGE"
