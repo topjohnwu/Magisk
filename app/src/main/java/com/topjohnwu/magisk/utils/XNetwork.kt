724893879f27a3c513e0df25c122960732ae0405
Viktor De Pasquale <info@depasquale.wiki>
2019-07-09 20:00:57 +0200

    Added option to intercept progress while copying files

--- app/src/main/java/com/topjohnwu/magisk/utils/XNetwork.kt
+++ app/src/main/java/com/topjohnwu/magisk/utils/XNetwork.kt
@@ -3,13 +3,36 @@ package com.topjohnwu.magisk.utils
 import android.content.Context
 import okhttp3.ResponseBody
 import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
 
-fun ResponseBody.writeToFile(context: Context, fileName: String): File {
+inline fun ResponseBody.writeToCachedFile(
+    context: Context,
+    fileName: String,
+    progress: (Long) -> Unit = {}
+): File {
     val file = File(context.cacheDir, fileName)
     withStreams(byteStream(), file.outputStream()) { inStream, outStream ->
-        inStream.copyTo(outStream)
+        inStream.copyToWithProgress(outStream, progress)
     }
     return file
 }
 
 fun ResponseBody.writeToString() = string()
+
+inline fun InputStream.copyToWithProgress(
+    out: OutputStream,
+    progressEmitter: (Long) -> Unit,
+    bufferSize: Int = DEFAULT_BUFFER_SIZE
+): Long {
+    var bytesCopied: Long = 0
+    val buffer = ByteArray(bufferSize)
+    var bytes = read(buffer)
+    while (bytes >= 0) {
+        out.write(buffer, 0, bytes)
+        bytesCopied += bytes
+        bytes = read(buffer)
+        progressEmitter(bytesCopied)
+    }
+    return bytesCopied
+}
