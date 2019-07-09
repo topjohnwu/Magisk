53f0eed9276390f5d7977e1bcea7f4ef8a356977
Viktor De Pasquale <info@depasquale.wiki>
2019-07-09 20:06:33 +0200

    Added several calls to FlashActivity so it manages its launch parameters by itself Its reach will be deepened further in the future commits

--- app/src/main/java/com/topjohnwu/magisk/ui/flash/FlashActivity.kt
+++ app/src/main/java/com/topjohnwu/magisk/ui/flash/FlashActivity.kt
@@ -1,11 +1,16 @@
 package com.topjohnwu.magisk.ui.flash
 
+import android.content.Context
+import android.content.Intent
+import androidx.core.net.toUri
+import com.topjohnwu.magisk.ClassMap
 import com.topjohnwu.magisk.Const
 import com.topjohnwu.magisk.R
 import com.topjohnwu.magisk.databinding.ActivityFlashBinding
 import com.topjohnwu.magisk.ui.base.MagiskActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
+import java.io.File
 
 open class FlashActivity : MagiskActivity<FlashViewModel, ActivityFlashBinding>() {
 
@@ -19,6 +24,27 @@ open class FlashActivity : MagiskActivity<FlashViewModel, ActivityFlashBinding>(
     override fun onBackPressed() {
         if (viewModel.loading) return
         super.onBackPressed()
+    }
+
+    companion object {
+
+        private fun intent(context: Context) = Intent(context, ClassMap[FlashActivity::class.java])
+
+        fun flashMagiskIntent(context: Context, file: File) = intent(context)
+            .setData(file.toUri())
+            .putExtra(Const.Key.FLASH_ACTION, Const.Value.FLASH_MAGISK)
+
+        fun flashMagisk(context: Context, file: File) =
+            context.startActivity(flashMagiskIntent(context, file))
+
+
+        fun flashModuleIntent(context: Context, file: File) = intent(context)
+            .setData(file.toUri())
+            .putExtra(Const.Key.FLASH_ACTION, Const.Value.FLASH_ZIP)
+
+        fun flashModule(context: Context, file: File) =
+            context.startActivity(flashModuleIntent(context, file))
+
     }
 
 }
