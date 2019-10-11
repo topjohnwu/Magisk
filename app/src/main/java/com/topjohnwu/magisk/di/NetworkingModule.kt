ff8460b36180f3edac9c6170ce811b796a26ba43
topjohnwu <topjohnwu@gmail.com>
2019-10-11 03:29:55 -0400

    Update dependencies

--- app/src/main/java/com/topjohnwu/magisk/di/NetworkingModule.kt
+++ app/src/main/java/com/topjohnwu/magisk/di/NetworkingModule.kt
@@ -9,6 +9,10 @@ import com.topjohnwu.magisk.data.network.GithubApiServices
 import com.topjohnwu.magisk.data.network.GithubRawServices
 import com.topjohnwu.magisk.net.Networking
 import com.topjohnwu.magisk.net.NoSSLv3SocketFactory
+import io.noties.markwon.Markwon
+import io.noties.markwon.html.HtmlPlugin
+import io.noties.markwon.image.ImagesPlugin
+import io.noties.markwon.image.network.OkHttpNetworkSchemeHandler
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
@@ -23,6 +27,7 @@ val networkingModule = module {
     single { createRetrofit(get()) }
     single { createApiService<GithubRawServices>(get(), Const.Url.GITHUB_RAW_URL) }
     single { createApiService<GithubApiServices>(get(), Const.Url.GITHUB_API_URL) }
+    single { createMarkwon(get(), get()) }
 }
 
 @Suppress("DEPRECATION")
@@ -66,4 +71,13 @@ inline fun <reified T> createApiService(retrofitBuilder: Retrofit.Builder, baseU
         .baseUrl(baseUrl)
         .build()
         .create(T::class.java)
+}
+
+fun createMarkwon(context: Context, okHttpClient: OkHttpClient): Markwon {
+    return Markwon.builder(context)
+            .usePlugin(HtmlPlugin.create())
+            .usePlugin(ImagesPlugin.create {
+                it.addSchemeHandler(OkHttpNetworkSchemeHandler.create(okHttpClient))
+            })
+            .build()
 }
