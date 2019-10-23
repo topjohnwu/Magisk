2be0cef446ea4b9b7b46e33488eaad176dbdb0d9
topjohnwu <topjohnwu@gmail.com>
2019-10-23 17:55:26 -0400

    Add proper intent filters to stub

--- shared/src/main/java/com/topjohnwu/magisk/utils/DynAPK.java
+++ shared/src/main/java/com/topjohnwu/magisk/utils/DynAPK.java
@@ -9,7 +9,7 @@ import static android.os.Build.VERSION.SDK_INT;
 
 public class DynAPK {
 
-    private static final int STUB_VERSION = 1;
+    private static final int STUB_VERSION = 2;
 
     // Indices of the object array
     private static final int STUB_VERSION_ENTRY = 0;
