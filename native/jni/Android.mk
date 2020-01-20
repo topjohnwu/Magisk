c3e77b1ec164861aa287bd57741a54e37362a939
topjohnwu <topjohnwu@gmail.com>
2020-01-20 20:48:05 +0800

    Add BusyBox SELinux support
    Close #1523

--- native/jni/Android.mk
+++ native/jni/Android.mk
@@ -14,6 +14,7 @@ LIBSYSTEMPROPERTIES := jni/systemproperties/include
 LIBUTILS := jni/utils/include
 LIBMINCRYPT := $(EXT_PATH)/mincrypt/include
 LIBXZ := $(EXT_PATH)/xz-embedded
+LIBPCRE2 := $(EXT_PATH)/pcre/include
 
 ########################
 # Binaries
