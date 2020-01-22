0dc9f5c324e8aeb4223aba343f45e50a1bc8aca3
topjohnwu <topjohnwu@gmail.com>
2020-01-23 02:34:18 +0800

    Rename some string IDs

--- app/src/main/java/com/topjohnwu/magisk/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/topjohnwu/magisk/ui/home/HomeViewModel.kt
@@ -43,18 +43,18 @@ class HomeViewModel(
     val stateManager = KObservableField(MagiskState.LOADING)
     val stateTextMagisk = Observer(stateMagisk) {
         when (stateMagisk.value) {
-            MagiskState.NOT_INSTALLED -> R.string.installed_error_md2.res()
-            MagiskState.UP_TO_DATE -> R.string.up_to_date_md2.res()
-            MagiskState.LOADING -> R.string.loading_md2.res()
-            MagiskState.OBSOLETE -> R.string.obsolete_md2.res()
+            MagiskState.NOT_INSTALLED -> R.string.installed_error.res()
+            MagiskState.UP_TO_DATE -> R.string.up_to_date.res()
+            MagiskState.LOADING -> R.string.loading.res()
+            MagiskState.OBSOLETE -> R.string.obsolete.res()
         }
     }
     val stateTextManager = Observer(stateManager) {
         when (stateManager.value) {
-            MagiskState.NOT_INSTALLED -> R.string.channel_error_md2.res()
-            MagiskState.UP_TO_DATE -> R.string.up_to_date_md2.res()
-            MagiskState.LOADING -> R.string.loading_md2.res()
-            MagiskState.OBSOLETE -> R.string.obsolete_md2.res()
+            MagiskState.NOT_INSTALLED -> R.string.channel_error.res()
+            MagiskState.UP_TO_DATE -> R.string.up_to_date.res()
+            MagiskState.LOADING -> R.string.loading.res()
+            MagiskState.OBSOLETE -> R.string.obsolete.res()
         }
     }
     val statePackageManager = packageName
