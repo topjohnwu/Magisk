9a8274130be805ed534090a48d12211ea7553ff1
topjohnwu <topjohnwu@gmail.com>
2020-02-11 20:54:23 -0800

    Manually set referenced resource ID for barriers

--- app/src/main/java/com/topjohnwu/magisk/ui/home/HomeFragment.kt
+++ app/src/main/java/com/topjohnwu/magisk/ui/home/HomeFragment.kt
@@ -1,8 +1,7 @@
 package com.topjohnwu.magisk.ui.home
 
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
+import android.os.Bundle
+import android.view.*
 import com.topjohnwu.magisk.R
 import com.topjohnwu.magisk.databinding.FragmentHomeMd2Binding
 import com.topjohnwu.magisk.model.navigation.Navigation
@@ -18,6 +17,24 @@ class HomeFragment : BaseUIFragment<HomeViewModel, FragmentHomeMd2Binding>() {
         super.onStart()
         activity.title = resources.getString(R.string.section_home)
         setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        // Set barrier reference IDs in code, since resource IDs will be stripped in release mode
+        binding.homeDeviceWrapper.homeDeviceTitleBarrier.referencedIds =
+            intArrayOf(R.id.home_device_action, R.id.home_device_title, R.id.home_device_icon)
+        binding.homeMagiskWrapper.homeMagiskTitleBarrier.referencedIds =
+            intArrayOf(R.id.home_magisk_action, R.id.home_magisk_title, R.id.home_magisk_icon)
+        binding.homeManagerWrapper.homeManagerTitleBarrier.referencedIds =
+            intArrayOf(R.id.home_manager_action, R.id.home_manager_title, R.id.home_manager_icon)
+
+        return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
