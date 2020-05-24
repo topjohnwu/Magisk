eb2959c680395c53d9f63b7a6408aa420299b91b
vvb2060 <vvb2060@gmail.com>
2020-05-24 13:15:43 +0800

    Use bootstrapDnsHosts

--- app/src/main/java/com/topjohnwu/magisk/di/NetworkingModule.kt
+++ app/src/main/java/com/topjohnwu/magisk/di/NetworkingModule.kt
@@ -21,6 +21,7 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.net.InetAddress
 
 val networkingModule = module {
     single { createOkHttpClient(get()) }
@@ -45,8 +46,21 @@ fun createOkHttpClient(context: Context): OkHttpClient {
         builder.sslSocketFactory(NoSSLv3SocketFactory())
     }
 
+    val bootstrapDnsHosts = listOf(
+            InetAddress.getByAddress("cloudflare-dns.com", byteArrayOf(162.toByte(), 159.toByte(), 36, 1)),
+            InetAddress.getByAddress("cloudflare-dns.com", byteArrayOf(162.toByte(), 159.toByte(), 46, 1)),
+            InetAddress.getByAddress("cloudflare-dns.com", byteArrayOf(1, 1, 1, 1)),
+            InetAddress.getByAddress("cloudflare-dns.com", byteArrayOf(1, 0, 0, 1)),
+            InetAddress.getByAddress("cloudflare-dns.com", byteArrayOf(162.toByte(), 159.toByte(), 132.toByte(), 53)),
+            InetAddress.getByAddress("cloudflare-dns.com", byteArrayOf(0x26, 0x06, 0x47, 0, 0x47, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x11, 0x11)),
+            InetAddress.getByAddress("cloudflare-dns.com", byteArrayOf(0x26, 0x06, 0x47, 0, 0x47, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x10, 0x01)),
+            InetAddress.getByAddress("cloudflare-dns.com", byteArrayOf(0x26, 0x06, 0x47, 0, 0x47, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x64)),
+            InetAddress.getByAddress("cloudflare-dns.com", byteArrayOf(0x26, 0x06, 0x47, 0, 0x47, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x64, 0x00))
+    )
+
     builder.dns(DnsOverHttps.Builder().client(builder.build())
-            .url(HttpUrl.get("https://162.159.36.1/dns-query")) //Cloudflare
+            .url(HttpUrl.get("https://cloudflare-dns.com/dns-query"))
+            .bootstrapDnsHosts(bootstrapDnsHosts)
             .build())
 
     return builder.build()
