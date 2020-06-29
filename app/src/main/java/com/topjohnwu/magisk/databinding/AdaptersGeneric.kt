a0b47f3ca39f37bded5bcb704431021638196932
topjohnwu <topjohnwu@gmail.com>
2020-06-29 05:25:54 -0700

    Precompute TextView in I/O thread for performance

--- app/src/main/java/com/topjohnwu/magisk/databinding/AdaptersGeneric.kt
+++ app/src/main/java/com/topjohnwu/magisk/databinding/AdaptersGeneric.kt
@@ -1,9 +1,14 @@
 package com.topjohnwu.magisk.databinding
 
 import android.view.View
+import android.widget.TextView
+import androidx.core.text.PrecomputedTextCompat
 import androidx.core.view.isGone
 import androidx.core.view.isInvisible
+import androidx.core.widget.TextViewCompat
 import androidx.databinding.BindingAdapter
+import com.topjohnwu.magisk.extensions.subscribeK
+import io.reactivex.Single
 
 @BindingAdapter("gone")
 fun setGone(view: View, gone: Boolean) {
@@ -23,4 +28,13 @@ fun setGoneUnless(view: View, goneUnless: Boolean) {
 @BindingAdapter("invisibleUnless")
 fun setInvisibleUnless(view: View, invisibleUnless: Boolean) {
     setInvisible(view, invisibleUnless.not())
+}
+
+@BindingAdapter("precomputedText")
+fun setPrecomputedText(tv: TextView, text: CharSequence) {
+    Single.fromCallable {
+        PrecomputedTextCompat.create(text, TextViewCompat.getTextMetricsParams(tv))
+    }.subscribeK {
+        TextViewCompat.setPrecomputedText(tv, it);
+    }
 }
