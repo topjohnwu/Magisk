8b43d01fba0badea5cc638fd727abbf7fa1d26d2
vvb2060 <vvb2060@gmail.com>
2020-10-15 02:00:10 +0800

    Add userspace reboot

--- app/src/main/java/com/topjohnwu/magisk/events/RebootEvent.kt
+++ app/src/main/java/com/topjohnwu/magisk/events/RebootEvent.kt
@@ -1,8 +1,11 @@
 package com.topjohnwu.magisk.events
 
+import android.os.Build
+import android.os.PowerManager
 import android.view.ContextThemeWrapper
 import android.view.MenuItem
 import android.widget.PopupMenu
+import androidx.core.content.getSystemService
 import com.topjohnwu.magisk.R
 import com.topjohnwu.magisk.core.base.BaseActivity
 import com.topjohnwu.superuser.Shell
@@ -13,6 +16,7 @@ object RebootEvent {
     private fun reboot(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.action_reboot_normal -> systemReboot()
+            R.id.action_reboot_userspace -> systemReboot("userspace")
             R.id.action_reboot_bootloader -> systemReboot("bootloader")
             R.id.action_reboot_download -> systemReboot("download")
             R.id.action_reboot_edl -> systemReboot("edl")
@@ -25,6 +29,9 @@ object RebootEvent {
     fun inflateMenu(activity: BaseActivity): PopupMenu {
         val themeWrapper = ContextThemeWrapper(activity, R.style.Foundation_PopupMenu)
         val menu = PopupMenu(themeWrapper, activity.findViewById(R.id.action_reboot))
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R &&
+            activity.getSystemService<PowerManager>()?.isRebootingUserspaceSupported == true)
+            menu.menu.getItem(R.id.action_reboot_userspace).isVisible = true
         activity.menuInflater.inflate(R.menu.menu_reboot, menu.menu)
         menu.setOnMenuItemClickListener(::reboot)
         return menu
