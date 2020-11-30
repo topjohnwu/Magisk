983401243965c74bc8be4687f9a8824f2fd4a573
vvb2060 <vvb2060@gmail.com>
2020-12-01 03:33:10 +0800

    Update SuPolicy
    Properly support SharedUserId, not use a random application.
    Handling **any** data sent by magiskd and handle exception properly.

--- app/src/main/java/com/topjohnwu/magisk/core/su/SuCallbackHandler.kt
+++ app/src/main/java/com/topjohnwu/magisk/core/su/SuCallbackHandler.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.os.Process
 import android.widget.Toast
 import com.topjohnwu.magisk.BuildConfig
 import com.topjohnwu.magisk.R
@@ -13,6 +12,7 @@ import com.topjohnwu.magisk.core.intent
 import com.topjohnwu.magisk.core.model.su.SuPolicy
 import com.topjohnwu.magisk.core.model.su.toLog
 import com.topjohnwu.magisk.core.model.su.toPolicy
+import com.topjohnwu.magisk.core.model.su.toUidPolicy
 import com.topjohnwu.magisk.di.ServiceLocator
 import com.topjohnwu.magisk.ktx.startActivity
 import com.topjohnwu.magisk.ktx.startActivityWithRoot
@@ -57,13 +57,6 @@ object SuCallbackHandler {
         }
     }
 
-    private fun Any?.toInt(): Int? {
-        return when (this) {
-            is Number -> this.toInt()
-            else -> null
-        }
-    }
-
     private fun handleRequest(context: Context, data: Bundle) {
         val intent = context.intent<SuRequestActivity>()
             .setAction(REQUEST)
@@ -78,25 +71,32 @@ object SuCallbackHandler {
         }
     }
 
+    // https://android.googlesource.com/platform/frameworks/base/+/547bf5487d52b93c9fe183aa6d56459c170b17a4
+    private fun Bundle.getIntComp(key: String, defaultValue: Int): Int {
+        val value = get(key) ?: return defaultValue
+        return when (value) {
+            is Int -> value
+            is Long -> value.toInt()
+            else -> defaultValue
+        }
+    }
+
     private fun handleLogging(context: Context, data: Bundle) {
-        val fromUid = data["from.uid"].toInt() ?: return
-        if (fromUid == Process.myUid())
-            return
+        val fromUid = data.getIntComp("from.uid", -1)
+        val notify = data.getBoolean("notify", true)
+        val allow = data.getIntComp("policy", SuPolicy.ALLOW)
 
         val pm = context.packageManager
+        val policy = runCatching { fromUid.toPolicy(pm, allow) }.getOrElse {
+            GlobalScope.launch { ServiceLocator.policyDB.delete(fromUid) }
+            fromUid.toUidPolicy(pm, allow)
+        }
 
-        val notify = data.getBoolean("notify", true)
-        val allow = data["policy"].toInt() ?: return
+        if (notify) notify(context, policy)
 
-        val policy = runCatching { fromUid.toPolicy(pm, allow) }.getOrElse { return }
-
-        if (notify)
-            notify(context, policy)
-
-        val toUid = data["to.uid"].toInt() ?: return
-        val pid = data["pid"].toInt() ?: return
-
-        val command = data.getString("command") ?: return
+        val toUid = data.getIntComp("to.uid", -1)
+        val pid = data.getIntComp("pid", -1)
+        val command = data.getString("command", "")
         val log = policy.toLog(
             toUid = toUid,
             fromPid = pid,
@@ -109,22 +109,19 @@ object SuCallbackHandler {
     }
 
     private fun handleNotify(context: Context, data: Bundle) {
-        val fromUid = data["from.uid"].toInt() ?: return
-        if (fromUid == Process.myUid())
-            return
+        val fromUid = data.getIntComp("from.uid", -1)
+        val allow = data.getIntComp("policy", SuPolicy.ALLOW)
 
         val pm = context.packageManager
-        val allow = data["policy"].toInt() ?: return
-
-        runCatching {
-            val policy = fromUid.toPolicy(pm, allow)
-            if (policy.policy >= 0)
-                notify(context, policy)
+        val policy = runCatching { fromUid.toPolicy(pm, allow) }.getOrElse {
+            GlobalScope.launch { ServiceLocator.policyDB.delete(fromUid) }
+            fromUid.toUidPolicy(pm, allow)
         }
+        notify(context, policy)
     }
 
     private fun notify(context: Context, policy: SuPolicy) {
-        if (policy.notification && Config.suNotification == Config.Value.NOTIFICATION_TOAST) {
+        if (Config.suNotification == Config.Value.NOTIFICATION_TOAST) {
             val resId = if (policy.policy == SuPolicy.ALLOW)
                 R.string.su_allow_toast
             else
