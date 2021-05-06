7702094053483b8943c34c851f9687a68274a4b0
topjohnwu <topjohnwu@gmail.com>
2021-05-06 11:37:21 -0700

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -223,7 +223,7 @@ dependencies {
     implementation("com.squareup.moshi:moshi:${vMoshi}")
     kapt("com.squareup.moshi:moshi-kotlin-codegen:${vMoshi}")
 
-    val vRoom = "2.3.0-rc01"
+    val vRoom = "2.3.0"
     implementation("androidx.room:room-runtime:${vRoom}")
     implementation("androidx.room:room-ktx:${vRoom}")
     kapt("androidx.room:room-compiler:${vRoom}")
@@ -238,9 +238,9 @@ dependencies {
     implementation("androidx.browser:browser:1.3.0")
     implementation("androidx.preference:preference:1.1.1")
     implementation("androidx.recyclerview:recyclerview:1.2.0")
-    implementation("androidx.fragment:fragment-ktx:1.3.2")
+    implementation("androidx.fragment:fragment-ktx:1.3.3")
     implementation("androidx.work:work-runtime-ktx:2.5.0")
-    implementation("androidx.transition:transition:1.4.0")
+    implementation("androidx.transition:transition:1.4.1")
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("com.google.android.material:material:1.3.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ buildscript {
     extra["vNav"] = vNav
 
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.3")
-        classpath(kotlin("gradle-plugin", version = "1.4.32"))
+        classpath("com.android.tools.build:gradle:4.2.0")
+        classpath(kotlin("gradle-plugin", version = "1.5.0"))
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${vNav}")
 
         // NOTE: Do not place your application dependencies here; they belong
