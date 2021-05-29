323bc41a7930b5c68059127c507b320595a47dd7
孟武.尼德霍格.龍 <7708801314520.tony@gmail.com>
2021-05-29 19:55:21 +0800

    Update update_binary.sh

--- scripts/update_binary.sh
+++ scripts/update_binary.sh
@@ -8,7 +8,7 @@ export BBBIN=$TMPDIR/busybox
 
 for arch in "x86_64" "x86" "arm64-v8a" "armeabi-v7a"; do
   unzip -o "$3" "lib/$arch/libbusybox.so" -d $TMPDIR >&2
-  chmod -R 755 $TMPDIR/libs
+  chmod -R 755 $TMPDIR/lib
   libpath="$TMPDIR/lib/$arch/libbusybox.so"
   if [ -x $libpath ] && $libpath >/dev/null 2>&1; then
     mv -f $libpath $BBBIN
