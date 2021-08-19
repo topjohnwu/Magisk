ffe47300a1352e32a611b96f28fdbdc0bf84623f
topjohnwu <topjohnwu@gmail.com>
2021-08-19 04:36:47 -0700

    Update recv/send fd function

--- native/jni/include/socket.hpp
+++ native/jni/include/socket.hpp
@@ -3,12 +3,14 @@
 #include <sys/un.h>
 #include <sys/socket.h>
 #include <string_view>
+#include <vector>
 
 socklen_t setup_sockaddr(sockaddr_un *sun, const char *name);
-int socket_accept(int sockfd, int timeout);
-void get_client_cred(int fd, struct ucred *cred);
+void get_client_cred(int fd, ucred *cred);
+std::vector<int> recv_fds(int sockfd);
 int recv_fd(int sockfd);
-void send_fd(int sockfd, int fd);
+int send_fds(int sockfd, const int *fds, int cnt);
+int send_fd(int sockfd, int fd);
 int read_int(int fd);
 int read_int_be(int fd);
 void write_int(int fd, int val);
