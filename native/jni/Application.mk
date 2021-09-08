a0ee79fd9f50928ab57253bc99a0cb7aefeeba51
HighW4y2H3ll <zhenghaohuu@gmail.com>
2021-09-08 01:02:49 -0400

    explicitly specify gold linker to avoid busybox lto linking issue on macos

--- native/jni/Application.mk
+++ native/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_ABI          := armeabi-v7a arm64-v8a x86 x86_64
 APP_CFLAGS       := -Wall -Oz -fomit-frame-pointer -flto
-APP_LDFLAGS      := -flto
+APP_LDFLAGS      := -flto -fuse-ld=gold
 APP_CPPFLAGS     := -std=c++17
 APP_STL          := none
 APP_PLATFORM     := android-16
