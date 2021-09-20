713ce4719b81788b8861b9c485bc0ff797df483a
Thonsi <10291398+thonsi@users.noreply.github.com>
2021-09-20 16:42:38 +0530

    Cleanup unused code

--- app/src/main/java/com/topjohnwu/magisk/utils/HideableBehavior.kt
+++
@@ -1,9 +0,0 @@
-package com.topjohnwu.magisk.utils
-
-import android.view.View
-
-interface HideableBehavior<V : View> {
-
-    fun setHidden(view: V, hide: Boolean, lockState: Boolean = false)
-
-}
