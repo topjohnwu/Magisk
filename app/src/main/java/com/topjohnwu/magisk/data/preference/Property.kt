7fb2587d45491b259a55c37a1a9faf24858fbd12
vvb2060 <vvb2060@gmail.com>
2022-01-15 19:52:13 +0800

    Merge into one file

--- app/src/main/java/com/topjohnwu/magisk/data/preference/Property.kt
+++ app/src/main/java/com/topjohnwu/magisk/data/preference/Property.kt
@@ -1,6 +1,9 @@
 package com.topjohnwu.magisk.data.preference
 
 import android.content.SharedPreferences
+import androidx.core.content.edit
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 abstract class Property {
 
@@ -18,4 +21,148 @@ abstract class Property {
     fun SharedPreferences.get(name: String, value: String) = getString(name, value) ?: value
     fun SharedPreferences.get(name: String, value: Set<String>) = getStringSet(name, value) ?: value
 
+}
+
+class BooleanProperty(
+    private val name: String,
+    private val default: Boolean,
+    private val commit: Boolean
+) : Property(), ReadWriteProperty<PreferenceModel, Boolean> {
+
+    override operator fun getValue(
+        thisRef: PreferenceModel,
+        property: KProperty<*>
+    ): Boolean {
+        val prefName = name.ifBlank { property.name }
+        return thisRef.prefs.get(prefName, default)
+    }
+
+    override operator fun setValue(
+        thisRef: PreferenceModel,
+        property: KProperty<*>,
+        value: Boolean
+    ) {
+        val prefName = name.ifBlank { property.name }
+        thisRef.prefs.edit(commit) { put(prefName, value) }
+    }
+}
+
+class FloatProperty(
+    private val name: String,
+    private val default: Float,
+    private val commit: Boolean
+) : Property(), ReadWriteProperty<PreferenceModel, Float> {
+
+    override operator fun getValue(
+        thisRef: PreferenceModel,
+        property: KProperty<*>
+    ): Float {
+        val prefName = name.ifBlank { property.name }
+        return thisRef.prefs.get(prefName, default)
+    }
+
+    override operator fun setValue(
+        thisRef: PreferenceModel,
+        property: KProperty<*>,
+        value: Float
+    ) {
+        val prefName = name.ifBlank { property.name }
+        thisRef.prefs.edit(commit) { put(prefName, value) }
+    }
+}
+
+class IntProperty(
+    private val name: String,
+    private val default: Int,
+    private val commit: Boolean
+) : Property(), ReadWriteProperty<PreferenceModel, Int> {
+
+    override operator fun getValue(
+        thisRef: PreferenceModel,
+        property: KProperty<*>
+    ): Int {
+        val prefName = name.ifBlank { property.name }
+        return thisRef.prefs.get(prefName, default)
+    }
+
+    override operator fun setValue(
+        thisRef: PreferenceModel,
+        property: KProperty<*>,
+        value: Int
+    ) {
+        val prefName = name.ifBlank { property.name }
+        thisRef.prefs.edit(commit) { put(prefName, value) }
+    }
+}
+
+class LongProperty(
+    private val name: String,
+    private val default: Long,
+    private val commit: Boolean
+) : Property(), ReadWriteProperty<PreferenceModel, Long> {
+
+    override operator fun getValue(
+        thisRef: PreferenceModel,
+        property: KProperty<*>
+    ): Long {
+        val prefName = name.ifBlank { property.name }
+        return thisRef.prefs.get(prefName, default)
+    }
+
+    override operator fun setValue(
+        thisRef: PreferenceModel,
+        property: KProperty<*>,
+        value: Long
+    ) {
+        val prefName = name.ifBlank { property.name }
+        thisRef.prefs.edit(commit) { put(prefName, value) }
+    }
+}
+
+class StringProperty(
+    private val name: String,
+    private val default: String,
+    private val commit: Boolean
+) : Property(), ReadWriteProperty<PreferenceModel, String> {
+
+    override operator fun getValue(
+        thisRef: PreferenceModel,
+        property: KProperty<*>
+    ): String {
+        val prefName = name.ifBlank { property.name }
+        return thisRef.prefs.get(prefName, default)
+    }
+
+    override operator fun setValue(
+        thisRef: PreferenceModel,
+        property: KProperty<*>,
+        value: String
+    ) {
+        val prefName = name.ifBlank { property.name }
+        thisRef.prefs.edit(commit) { put(prefName, value) }
+    }
+}
+
+class StringSetProperty(
+    private val name: String,
+    private val default: Set<String>,
+    private val commit: Boolean
+) : Property(), ReadWriteProperty<PreferenceModel, Set<String>> {
+
+    override operator fun getValue(
+        thisRef: PreferenceModel,
+        property: KProperty<*>
+    ): Set<String> {
+        val prefName = name.ifBlank { property.name }
+        return thisRef.prefs.get(prefName, default)
+    }
+
+    override operator fun setValue(
+        thisRef: PreferenceModel,
+        property: KProperty<*>,
+        value: Set<String>
+    ) {
+        val prefName = name.ifBlank { property.name }
+        thisRef.prefs.edit(commit) { put(prefName, value) }
+    }
 }
