dcbb4eabb50e6a338b6e63fd9dcadc7539bf134e
vvb2060 <vvb2060@gmail.com>
2022-01-25 10:29:54 +0800

    Fix string

--- app/src/main/java/com/topjohnwu/magisk/events/dialog/RestoreAppDialog.kt
+++ app/src/main/java/com/topjohnwu/magisk/events/dialog/RestoreAppDialog.kt
@@ -10,11 +10,11 @@ class RestoreAppDialog : DialogEvent() {
             setTitle(R.string.settings_restore_app_title)
             setMessage(R.string.restore_app_confirmation)
             setButton(MagiskDialog.ButtonType.POSITIVE) {
-                text = R.string.yes
+                text = android.R.string.ok
                 onClick { HideAPK.restore(dialog.ownerActivity!!) }
             }
             setButton(MagiskDialog.ButtonType.NEGATIVE) {
-                text = R.string.no
+                text = android.R.string.cancel
             }
             setCancelable(true)
         }
