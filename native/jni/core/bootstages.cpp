b59e7bdda65d1fd4bb45573a35f638747bf1d3f5
LoveSy <shana@zju.edu.cn>
2022-02-03 20:14:57 +0800

    Don't use `getmntent_r` from system's libc
    Fix #5354

--- native/jni/core/bootstages.cpp
+++ native/jni/core/bootstages.cpp
@@ -39,7 +39,7 @@ bool zygisk_enabled = false;
 }
 
 #define mount_mirror(part, flag) \
-else if (MNT_DIR_IS("/" #part) && me->mnt_type != "tmpfs"sv && me->mnt_type != "overlay"sv && lstat(me->mnt_dir, &st) == 0) \
+(MNT_DIR_IS("/" #part) && me->mnt_type != "tmpfs"sv && me->mnt_type != "overlay"sv && lstat(me->mnt_dir, &st) == 0) \
     do_mount_mirror(part, flag)
 
 #define link_mirror(part) \
@@ -50,7 +50,7 @@ if (access("/system/" #part, F_OK) == 0 && access(buf1, F_OK) != 0) { \
 }
 
 #define link_orig_dir(dir, part) \
-else if (MNT_DIR_IS(dir) && me->mnt_type != "tmpfs"sv && me->mnt_type != "overlay"sv) { \
+(MNT_DIR_IS(dir) && me->mnt_type != "tmpfs"sv && me->mnt_type != "overlay"sv) { \
     SETMIR(buf1, part); \
     rmdir(buf1); \
     xsymlink(dir, buf1); \
@@ -66,17 +66,16 @@ static void mount_mirrors() {
     LOGI("* Mounting mirrors\n");
 
     parse_mnt("/proc/mounts", [&](mntent *me) {
-        struct stat st;
-        if (0) {}
-        mount_mirror(system, MS_RDONLY)
-        mount_mirror(vendor, MS_RDONLY)
-        mount_mirror(product, MS_RDONLY)
-        mount_mirror(system_ext, MS_RDONLY)
-        mount_mirror(data, 0)
-        link_orig(cache)
-        link_orig(metadata)
-        link_orig(persist)
-        link_orig_dir("/mnt/vendor/persist", persist)
+        struct stat st{};
+        if mount_mirror(system, MS_RDONLY)
+        else if mount_mirror(vendor, MS_RDONLY)
+        else if mount_mirror(product, MS_RDONLY)
+        else if mount_mirror(system_ext, MS_RDONLY)
+        else if mount_mirror(data, 0)
+        else if link_orig(cache)
+        else if link_orig(metadata)
+        else if link_orig(persist)
+        else if link_orig_dir("/mnt/vendor/persist", persist)
         else if (SDK_INT >= 24 && MNT_DIR_IS("/proc") && !strstr(me->mnt_opts, "hidepid=2")) {
             xmount(nullptr, "/proc", nullptr, MS_REMOUNT, "hidepid=2,gid=3009");
         }
--- native/jni/utils/compat/compat.cpp
+++ native/jni/utils/compat/compat.cpp
@@ -53,32 +53,6 @@ ssize_t getline(char **buf, size_t *bufsiz, FILE *fp) {
     return getdelim(buf, bufsiz, '\n', fp);
 }
 
-// Original source: https://android.googlesource.com/platform/bionic/+/master/libc/bionic/mntent.cpp
-// License: AOSP, full copyright notice please check original source
-
-struct mntent *getmntent_r(FILE *fp, struct mntent *e, char *buf, int buf_len) {
-    memset(e, 0, sizeof(*e));
-    while (fgets(buf, buf_len, fp) != nullptr) {
-        // Entries look like "proc /proc proc rw,nosuid,nodev,noexec,relatime 0 0".
-        // That is: mnt_fsname mnt_dir mnt_type mnt_opts 0 0.
-        int fsname0, fsname1, dir0, dir1, type0, type1, opts0, opts1;
-        if (sscanf(buf, " %n%*s%n %n%*s%n %n%*s%n %n%*s%n %d %d",
-                   &fsname0, &fsname1, &dir0, &dir1, &type0, &type1, &opts0, &opts1,
-                   &e->mnt_freq, &e->mnt_passno) == 2) {
-            e->mnt_fsname = &buf[fsname0];
-            buf[fsname1] = '\0';
-            e->mnt_dir = &buf[dir0];
-            buf[dir1] = '\0';
-            e->mnt_type = &buf[type0];
-            buf[type1] = '\0';
-            e->mnt_opts = &buf[opts0];
-            buf[opts1] = '\0';
-            return e;
-        }
-    }
-    return nullptr;
-}
-
 FILE *setmntent(const char *path, const char *mode) {
     return fopen(path, mode);
 }
