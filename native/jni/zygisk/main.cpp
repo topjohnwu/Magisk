d4fe8632ecc038196afc42ed8d0cae28189248d8
topjohnwu <topjohnwu@gmail.com>
2022-05-31 22:24:13 -0700

    Support SELinux disabled on debug builds

--- native/jni/zygisk/main.cpp
+++ native/jni/zygisk/main.cpp
@@ -6,6 +6,7 @@
 #include <base.hpp>
 #include <socket.hpp>
 #include <daemon.hpp>
+#include <selinux.hpp>
 
 #include "zygisk.hpp"
 
@@ -17,7 +18,14 @@ int app_process_main(int argc, char *argv[]) {
     char buf[PATH_MAX];
 
     bool zygote = false;
-    if (auto fp = open_file("/proc/self/attr/current", "r")) {
+    if (!selinux_enabled()) {
+        for (int i = 0; i < argc; ++i) {
+            if (argv[i] == "--zygote"sv) {
+                zygote = true;
+                break;
+            }
+        }
+    } else if (auto fp = open_file("/proc/self/attr/current", "r")) {
         fscanf(fp.get(), "%s", buf);
         zygote = (buf == "u:r:zygote:s0"sv);
     }
