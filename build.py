823b7cab6c55025bf5efa896a162f7a78325806e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-31 22:33:36 +0000

    Adding tarfile member sanitization to extractall()

--- build.py
+++ build.py
@@ -466,7 +466,26 @@ def setup_ndk(args):
     header(f'* Downloading and extracting {ndk_archive}')
     with urllib.request.urlopen(url) as response:
         with tarfile.open(mode='r|gz', fileobj=response) as tar:
-            tar.extractall(ndk_root)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, ndk_root)
 
     rm_rf(ndk_path)
     mv(op.join(ndk_root, f'ondk-{ndk_ver}'), ndk_path)
