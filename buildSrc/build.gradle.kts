7fe9db90a1a9512db74bb873f8f5d28243afdc78
topjohnwu <topjohnwu@gmail.com>
2023-01-18 12:52:04 +0800

    Update AGP

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -25,7 +25,7 @@ tasks.withType<KotlinCompile> {
 
 dependencies {
     implementation(kotlin("gradle-plugin", "1.8.0"))
-    implementation("com.android.tools.build:gradle:7.3.1")
+    implementation("com.android.tools.build:gradle:7.4.0")
     implementation("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
     implementation("io.michaelrocks:paranoid-gradle-plugin:0.3.7")
     implementation("org.eclipse.jgit:org.eclipse.jgit:6.4.0.202211300538-r")
