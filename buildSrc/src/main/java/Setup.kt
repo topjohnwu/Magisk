15e20891589e0f7b0a93e86756219963b0941754
LoveSy <shana@zju.edu.cn>
2023-01-23 14:43:08 +0800

    Avoid duplicate comments to stub

--- buildSrc/src/main/java/Setup.kt
+++ buildSrc/src/main/java/Setup.kt
@@ -135,10 +135,15 @@ private fun Project.setupAppCommon() {
     android.applicationVariants.all {
         val projectName = project.name.toLowerCase(Locale.ROOT)
         val variantCapped = name.capitalize(Locale.ROOT)
-        tasks.getByPath(":$projectName:package$variantCapped").doLast {
-            val apk = outputs.files.asFileTree.filter { it.name.endsWith(".apk") }.singleFile
-            val comment = "version=${Config.version}\nversionCode=${Config.versionCode}"
-            addComment(apk, signingConfig, android.defaultConfig.minSdk!!, comment)
+        tasks.getByPath(":$projectName:package$variantCapped").apply {
+            outputs.upToDateWhen {
+                false
+            }
+            doLast {
+                val apk = outputs.files.asFileTree.filter { it.name.endsWith(".apk") }.singleFile
+                val comment = "version=${Config.version}\nversionCode=${Config.versionCode}"
+                addComment(apk, signingConfig, android.defaultConfig.minSdk!!, comment)
+            }
         }
     }
 }
