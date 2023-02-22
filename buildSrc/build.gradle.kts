996a8570967f4455e782d61501244f1abce23720
LoveSy <shana@zju.edu.cn>
2023-02-22 21:50:40 +0800

    Upgrade kotlin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,7 +16,7 @@ gradlePlugin {
 }
 
 dependencies {
-    implementation(kotlin("gradle-plugin", "1.8.0"))
+    implementation(kotlin("gradle-plugin", "1.8.10"))
     implementation("com.android.tools.build:gradle:8.0.0-beta03")
     implementation("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
     implementation("org.lsposed.lsparanoid:gradle-plugin:0.5.0")
