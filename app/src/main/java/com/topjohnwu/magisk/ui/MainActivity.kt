cbbb2b51d2a6b3d33ed5663e8f12cd6da313759a
pranshoe <pranshu.goyal71@gmail.com>
2023-03-11 11:42:14 +0530

    ui: MainActivity: Add back toolbar animations

--- app/src/main/java/com/topjohnwu/magisk/ui/MainActivity.kt
+++ app/src/main/java/com/topjohnwu/magisk/ui/MainActivity.kt
@@ -147,7 +147,7 @@ class MainActivity : SplashActivity<ActivityMainMd2Binding>() {
     }
 
     fun invalidateToolbar() {
-        //binding.mainToolbar.startAnimations()
+        binding.mainToolbar.startAnimations()
         binding.mainToolbar.invalidate()
     }
 
