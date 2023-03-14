7f7da87ea75eb388bd4555a402d75d496eabe417
vvb2060 <vvb2060@gmail.com>
2023-03-15 00:24:33 +0800

    Update SELinux policy

--- native/src/core/restorecon.cpp
+++ native/src/core/restorecon.cpp
@@ -10,7 +10,6 @@ using namespace std;
 #define SYSTEM_CON  "u:object_r:system_file:s0"
 #define ADB_CON     "u:object_r:adb_data_file:s0"
 #define ROOT_CON    "u:object_r:rootfs:s0"
-#define EXEC_CON    "u:object_r:" SEPOL_EXEC_TYPE ":s0"
 
 static void restore_syscon_from_null(int dirfd) {
     struct dirent *entry;
@@ -82,16 +81,11 @@ void restore_tmpcon() {
     if (MAGISKTMP == "/sbin")
         setfilecon(MAGISKTMP.data(), ROOT_CON);
     else
-        chmod(MAGISKTMP.data(), 0700);
+        chmod(MAGISKTMP.data(), 0711);
 
     auto dir = xopen_dir(MAGISKTMP.data());
     int dfd = dirfd(dir.get());
 
     for (dirent *entry; (entry = xreaddir(dir.get()));)
         setfilecon_at(dfd, entry->d_name, SYSTEM_CON);
-
-    if (SDK_INT >= 26) {
-        string magisk = MAGISKTMP + "/magisk";
-        setfilecon(magisk.data(), EXEC_CON);
-    }
 }
