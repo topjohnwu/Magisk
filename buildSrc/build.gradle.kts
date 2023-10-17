dd8c28b1cb0dcf057f8d191595eb9f74a66a0eb9
topjohnwu <topjohnwu@gmail.com>
2023-10-16 17:25:57 -0700

    Upgrade AGP

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,7 +17,7 @@ gradlePlugin {
 
 dependencies {
     implementation(kotlin("gradle-plugin", "1.9.0"))
-    implementation("com.android.tools.build:gradle:8.1.1")
+    implementation("com.android.tools.build:gradle:8.1.2")
     implementation("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.1")
     implementation("org.lsposed.lsparanoid:gradle-plugin:0.5.2")
     implementation("org.eclipse.jgit:org.eclipse.jgit:6.5.0.202303070854-r")
