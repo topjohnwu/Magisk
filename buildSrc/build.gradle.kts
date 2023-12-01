4e999970132a325e829dd28ccbbe23c049e0db37
LoveSy <shana@zju.edu.cn>
2023-12-01 19:46:43 +0800

    Upgrade AGP

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,7 +17,7 @@ gradlePlugin {
 
 dependencies {
     implementation(embeddedKotlin("gradle-plugin"))
-    implementation("com.android.tools.build:gradle:8.1.4")
+    implementation("com.android.tools.build:gradle:8.2.0")
     implementation("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.5")
     implementation("org.lsposed.lsparanoid:gradle-plugin:0.5.2")
     implementation("org.eclipse.jgit:org.eclipse.jgit:6.7.0.202309050840-r")
