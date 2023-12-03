de3116293d7d03cbd6e89daa7d09ccddc3df3721
LoveSy <shana@zju.edu.cn>
2023-12-03 22:13:06 +0800

    Fix native symbol strips
    `ndkVersion` is also needed by app for striping native symbols.
    Set it in `setupCommon` instead.

--- buildSrc/src/main/java/Setup.kt
+++ buildSrc/src/main/java/Setup.kt
@@ -72,6 +72,7 @@ fun Project.setupCommon() {
         compileSdkVersion(34)
         buildToolsVersion = "34.0.0"
         ndkPath = "$sdkDirectory/ndk/magisk"
+        ndkVersion = "26.1.10909125"
 
         defaultConfig {
             minSdk = 23
