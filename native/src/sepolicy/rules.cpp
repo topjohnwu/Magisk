88ddbe567397670ea4413fa5e6e41a236285891b
bmax <bmax@placeholder.com>
2023-12-24 20:19:28 +0800

    sepolicy rules: compat for APatch

--- native/src/sepolicy/rules.cpp
+++ native/src/sepolicy/rules.cpp
@@ -9,72 +9,25 @@ void sepolicy::magisk_rules() {
     // Temp suppress warnings
     set_log_level_state(LogLevel::Warn, false);
 
-    // Prevent anything to change sepolicy except ourselves
-    deny(ALL, "kernel", "security", "load_policy");
-
     type(SEPOL_PROC_DOMAIN, "domain");
     permissive(SEPOL_PROC_DOMAIN);  /* Just in case something is missing */
+
     typeattribute(SEPOL_PROC_DOMAIN, "mlstrustedsubject");
     typeattribute(SEPOL_PROC_DOMAIN, "netdomain");
     typeattribute(SEPOL_PROC_DOMAIN, "bluetoothdomain");
+
     type(SEPOL_FILE_TYPE, "file_type");
     typeattribute(SEPOL_FILE_TYPE, "mlstrustedobject");
-    type(SEPOL_LOG_TYPE, "file_type");
-    typeattribute(SEPOL_LOG_TYPE, "mlstrustedobject");
 
-    // Make our root domain unconstrained
     allow(SEPOL_PROC_DOMAIN, ALL, ALL, ALL);
+
     // Allow us to do any ioctl
     if (impl->db->policyvers >= POLICYDB_VERSION_XPERMS_IOCTL) {
         allowxperm(SEPOL_PROC_DOMAIN, ALL, "blk_file", ALL_XPERM);
         allowxperm(SEPOL_PROC_DOMAIN, ALL, "fifo_file", ALL_XPERM);
         allowxperm(SEPOL_PROC_DOMAIN, ALL, "chr_file", ALL_XPERM);
+        allowxperm(SEPOL_PROC_DOMAIN, ALL, "file", ALL_XPERM);
     }
-
-    // Create unconstrained file type
-    allow(ALL, SEPOL_FILE_TYPE, "file", ALL);
-    allow(ALL, SEPOL_FILE_TYPE, "dir", ALL);
-    allow(ALL, SEPOL_FILE_TYPE, "fifo_file", ALL);
-    allow(ALL, SEPOL_FILE_TYPE, "chr_file", ALL);
-    allow(ALL, SEPOL_FILE_TYPE, "lnk_file", ALL);
-    allow(ALL, SEPOL_FILE_TYPE, "sock_file", ALL);
-
-    // Only allow zygote to open log pipe
-    allow("zygote", SEPOL_LOG_TYPE, "fifo_file", "open");
-    allow("zygote", SEPOL_LOG_TYPE, "fifo_file", "read");
-    // Allow all processes to output logs
-    allow("domain", SEPOL_LOG_TYPE, "fifo_file", "write");
-
-    // Allow these processes to access MagiskSU and output logs
-    const char *clients[] {
-        "zygote", "shell", "system_app", "platform_app",
-        "priv_app", "untrusted_app", "untrusted_app_all"
-    };
-    for (auto type: clients) {
-        if (!exists(type))
-            continue;
-        allow(type, SEPOL_PROC_DOMAIN, "unix_stream_socket", "connectto");
-        allow(type, SEPOL_PROC_DOMAIN, "unix_stream_socket", "getopt");
-    }
-
-    // Let everyone access tmpfs files (for SAR sbin overlay)
-    allow(ALL, "tmpfs", "file", ALL);
-
-    // Allow magiskinit daemon to handle mock selinuxfs
-    allow("kernel", "tmpfs", "fifo_file", "write");
-
-    // For relabelling files
-    allow("rootfs", "labeledfs", "filesystem", "associate");
-    allow(SEPOL_FILE_TYPE, "pipefs", "filesystem", "associate");
-    allow(SEPOL_FILE_TYPE, "devpts", "filesystem", "associate");
-
-    // Let init transit to SEPOL_PROC_DOMAIN
-    allow("kernel", "kernel", "process", "setcurrent");
-    allow("kernel", SEPOL_PROC_DOMAIN, "process", "dyntransition");
-
-    // Let init run stuffs
-    allow("kernel", SEPOL_PROC_DOMAIN, "fd", "use");
-    allow("init", SEPOL_PROC_DOMAIN, "process", ALL);
 
     // suRights
     allow("servicemanager", SEPOL_PROC_DOMAIN, "dir", "search");
@@ -109,22 +62,6 @@ void sepolicy::magisk_rules() {
 
     // Allow all binder transactions
     allow(ALL, SEPOL_PROC_DOMAIN, "binder", ALL);
-
-    // For changing file context
-    allow("rootfs", "tmpfs", "filesystem", "associate");
-
-    // Zygisk rules
-    allow("zygote", "zygote", "capability", "sys_resource");  // prctl PR_SET_MM
-    allow("zygote", "zygote", "process", "execmem");
-    allow("zygote", "fs_type", "filesystem", "unmount");
-    allow("system_server", "system_server", "process", "execmem");
-
-    // Shut llkd up
-    dontaudit("llkd", SEPOL_PROC_DOMAIN, "process", "ptrace");
-
-    // Keep /data/adb/* context
-    deny("init", "adb_data_file", "dir", "search");
-    deny("vendor_init", "adb_data_file", "dir", "search");
 
 #if 0
     // Remove all dontaudit in debug mode
