346e173f654d271195d0346058e87d0940d816a2
canyie <a1364259@163.com>
2024-03-03 15:15:42 +0800

    Reverse format template argument order

--- native/src/init/rootdir.rs
+++ native/src/init/rootdir.rs
@@ -15,21 +15,21 @@ pub fn inject_magisk_rc(fd: RawFd, tmp_dir: &Utf8CStr) {
         r#"
 on post-fs-data
     start logd
-    exec {1} 0 0 -- {0}/magisk --post-fs-data
+    exec {0} 0 0 -- {1}/magisk --post-fs-data
 
 on property:vold.decrypt=trigger_restart_framework
-    exec {1} 0 0 -- {0}/magisk --service
+    exec {0} 0 0 -- {1}/magisk --service
 
 on nonencrypted
-    exec {1} 0 0 -- {0}/magisk --service
+    exec {0} 0 0 -- {1}/magisk --service
 
 on property:sys.boot_completed=1
-    exec {1} 0 0 -- {0}/magisk --boot-complete
+    exec {0} 0 0 -- {1}/magisk --boot-complete
 
 on property:init.svc.zygote=stopped
-    exec {1} 0 0 -- {0}/magisk --zygote-restart
+    exec {0} 0 0 -- {1}/magisk --zygote-restart
 "#,
-        tmp_dir, "u:r:magisk:s0"
+        "u:r:magisk:s0", tmp_dir
     )
     .ok();
 
