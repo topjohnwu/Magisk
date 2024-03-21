f3301df4e9b2d56e674385d5104fa9a975c4bdb7
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2024-03-21 15:18:25 +0800

    Upgrade AGP to 8.3.1

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,7 +17,7 @@ gradlePlugin {
 
 dependencies {
     implementation(embeddedKotlin("gradle-plugin"))
-    implementation("com.android.tools.build:gradle:8.3.0")
+    implementation("com.android.tools.build:gradle:8.3.1")
     implementation("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
     implementation("org.lsposed.lsparanoid:gradle-plugin:0.5.2")
     implementation("org.eclipse.jgit:org.eclipse.jgit:6.7.0.202309050840-r")
