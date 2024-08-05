721adcac5f268c5df01e4d0094cf00ff86979e9c
Peter Noël Muller <100597184+peternmuller@users.noreply.github.com>
2024-08-05 22:29:45 +0200

    Require Magisk v25.2 for modules
    It has been long enough

--- scripts/module_installer.sh
+++ scripts/module_installer.sh
@@ -11,7 +11,7 @@ ui_print() { echo "$1"; }
 
 require_new_magisk() {
   ui_print "*******************************"
-  ui_print " Please install Magisk v20.4+! "
+  ui_print " Please install Magisk v25.2+! "
   ui_print "*******************************"
   exit 1
 }
@@ -27,7 +27,7 @@ mount /data 2>/dev/null
 
 [ -f /data/adb/magisk/util_functions.sh ] || require_new_magisk
 . /data/adb/magisk/util_functions.sh
-[ $MAGISK_VER_CODE -lt 20400 ] && require_new_magisk
+[ $MAGISK_VER_CODE -lt 25200 ] && require_new_magisk
 
 install_module
 exit 0
