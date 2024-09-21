07ee12da343a82e244f07791ed2ca77193ad2f16
残页 <a1364259@163.com>
2024-09-21 21:36:20 +0800

    Check sepolicy database version in add_xperm_rule
    Fix #8344

--- native/src/sepolicy/sepolicy.cpp
+++ native/src/sepolicy/sepolicy.cpp
@@ -271,6 +271,10 @@ bool sepol_impl::add_rule(const char *s, const char *t, const char *c, const cha
 #define ioctl_func(x) (x & 0xFF)
 
 void sepol_impl::add_xperm_rule(type_datum_t *src, type_datum_t *tgt, class_datum_t *cls, const Xperm &p, int effect) {
+    if (db->policyvers < POLICYDB_VERSION_XPERMS_IOCTL) {
+        LOGE("policy version %u does not support ioctl extended permissions rules\n", db->policyvers);
+        return;
+    }
     if (src == nullptr) {
         for_each_attr(db->p_types.table, [&](type_datum_t *type) {
             add_xperm_rule(type, tgt, cls, p, effect);
