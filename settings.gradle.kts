86468845b2086c4f223013ef45974c0615a0f9d2
Kent1367 <mohsen.laleh28@gmail.com>
2024-11-22 02:23:18 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,7 @@
-@Suppress("UnstableApiUsage")
-dependencyResolutionManagement {
-    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-}
-rootProject.name = "Magisk"
-include(":app:apk", ":app:core", ":app:shared", ":app:stub", ":native")
+
+dependencyResolutionManagement 
+    repositoriesMode = google
+    repositories
+        google
+rootProject.name = Magisk
+include:app:apk
