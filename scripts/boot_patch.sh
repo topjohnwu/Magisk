74c44c3364aa526fa6c6c312aaef21f035c546d2
NeutroGe <60129797+NeutroGe@users.noreply.github.com>
2025-03-08 08:44:41 +0100

    Update boot_patch.sh

--- scripts/boot_patch.sh
+++ scripts/boot_patch.sh
@@ -119,7 +119,11 @@ else
    if find . -name "*.cpio" | grep -vF "./ramdisk.cpio" >null; then NONCOMPLIANT=1; fi #searching for any cpio file other than ./ramdisk.cpio
    if [ $NONCOMPLIANT -eq 1 ]; then 
      ui_print "- This boot image contains non standard cpio:"
-     find . -name "*.cpio" | grep -vF "./ramdisk.cpio" | xargs ui_print
+     find . -name "*.cpio" | grep -vF "./ramdisk.cpio" > tmp.log
+     while read p; do
+       ui_print "- $p"
+     done <tmp.log
+     rm tmp.log
      abort "! This boot image is unsupported"
    fi  
   ui_print "- No ramdisk file in the root directory"
