793507afd1d45163c12588336846f8799b8b58da
Howard Wu <HowardWu20@outlook.com>
2025-04-28 09:51:05 +0800

    apt-get update before install to fix ci

--- scripts/cuttlefish.sh
+++ scripts/cuttlefish.sh
@@ -20,6 +20,7 @@ run_cvd_bin() {
 
 setup_env() {
   curl -LO https://github.com/topjohnwu/magisk-files/releases/download/files/cuttlefish-base_1.2.0_amd64.deb
+  sudo apt-get update
   sudo dpkg -i ./cuttlefish-base_*_*64.deb || sudo apt-get install -f
   rm cuttlefish-base_*_*64.deb
   echo 'KERNEL=="kvm", GROUP="kvm", MODE="0666", OPTIONS+="static_node=kvm"' | sudo tee /etc/udev/rules.d/99-kvm4all.rules
