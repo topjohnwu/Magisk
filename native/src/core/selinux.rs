810174ef734eb67c36e362d854f98b212386e599
Wang Han <416810799@qq.com>
2025-06-17 20:44:52 +0800

    Ignore set_context() error if magisktmp is /sbin
    recreate_sbin() will bind mount original files in /sbin to tmpfs /sbin,
    so we have no choice but just log here to let the loop continue.

--- native/src/core/selinux.rs
+++ native/src/core/selinux.rs
@@ -85,7 +85,7 @@ pub(crate) fn restore_tmpcon() -> LoggedResult<()> {
     while let Some(ref e) = dir.read()? {
         if !e.is_symlink() {
             e.resolve_path(&mut path)?;
-            path.set_secontext(SYSTEM_CON)?;
+            path.set_secontext(SYSTEM_CON).log_ok();
         }
     }
 
