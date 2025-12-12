ba5f3cb1825eee2f5287b966d30aaa3fd1cce7b4
topjohnwu <topjohnwu@gmail.com>
2025-12-11 22:44:06 -0800

    Update to ONDK r29.4

--- build.py
+++ build.py
@@ -80,7 +80,7 @@ support_targets = {"magisk", "magiskinit", "magiskboot", "magiskpolicy", "resetp
 default_targets = support_targets - {"resetprop"}
 rust_targets = default_targets.copy()
 clean_targets = {"native", "cpp", "rust", "app"}
-ondk_version = "r29.3"
+ondk_version = "r29.4"
 
 # Global vars
 config = {}
